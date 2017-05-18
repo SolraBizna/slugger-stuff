da41486961b3f91e4ddb3b1c13a6bb70f51396b7
Solra Bizna <solra@bizna.name>
2017-05-18 11:36:48 -0600

    Implemented slugger-snap

---
+++ slugger-snap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo Usage: slugger-snap /base/machine
+    exit 1
+fi
+
+if [ ! -e "$1/current" ]; then
+    echo "slugger-snap: current directory didn't exist, doing nothing"
+    exit 0
+fi
+
+NOW=`date -u +%Y.%m.%d-%H%M.%S`
+
+if [ -e "$1/@$NOW" ]; then
+    echo "Already exists, try again later"
+    exit 1
+fi
+
+rm "$1/latest"
+mv "$1/current" "$1/@$NOW"
+ln -s "@$NOW" "$1"/latest
